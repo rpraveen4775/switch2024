fc1c6a76fcfc18b4b4ca9d7266d7b3c974c355c3
rpraveen4775 <rpraveenverma@gmail.com>
2024-06-16 11:40:30 +0530

    Correcting fibo

--- Backtracking/fibonacci.js
+++ Backtracking/fibonacci.js
@@ -2,17 +2,17 @@
 // Given length of fibbonacci . function should print it
 
 
-let number = 10;
+let number = 5;
 // 0 1 1 2 3 5 8 13 21 34
 
 let fib = function(n){
-    if(n==1){
+    if(n==0){
         return 0;
     } 
-    if (n == 2 ){
+    if (n == 1 ){
         return 1;
     }
     return  fib(n-1) + fib(n-2)
 }
 
-console.log(fib(10));
+console.log(fib(5));
